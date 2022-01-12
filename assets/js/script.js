729b38b9ff6cc9a7f41fec9ad524988d21a677c9
F1_Fan_GoMax! <95992081+bdibil@users.noreply.github.com>
2022-01-12 01:08:11 -0800

    minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,12 @@ var cityData = {};
 var cityList = {};
 var cityArray = [];
 
-
-
 var currentTime = ""
 var currentCity = "Bothell";
 var APIkey = "&APPID=530a0c5e5acc988437cc5fa97607f79a";
 var units = "&units=imperial"
+var dummy = 0
+
 
 // for current conditions
 var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="
@@ -25,7 +25,6 @@ var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="
 // for next 5 days
 var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q="
 
-var dummy = 0
 
 
 function init() {
@@ -43,10 +42,7 @@ searchBtn.on('click', function () {
         currentCity = userCity.value
     }
     fullUrl = requestUrl + currentCity + APIkey + units;
-    // localStorage.setItem("cityList", JSON.stringify(cityList))
-    // var storedCity = JSON.parse(localStorage.getItem("cityList"));
-    // cityArray.unshift = storedCity
-    // console.log(cityArray)
+
     getApi(fullUrl)
 });
 
@@ -75,14 +71,16 @@ function display() {
     tempEl.textContent = "Temp: " + cityData.main.temp + " °F"
     windEl.textContent = "Wind: " + cityData.wind.speed + " MPH"
     humEl.textContent = "Humidity: " + cityData.main.humidity + " %"
-
-
-
-    // uvEl.textContent = "Temp: " + cityData.main.temp + " °F"
-
-
-    // tempEl.textContent = cityData.city
 }
+
+
+
+
+
+////////////////        OLD CODE  below           ///////////
+
+
+
 
 // console.log(requestUrl)
 // console.log(city)
@@ -94,32 +92,6 @@ function display() {
 
 // b1.addEventListener('click', test());
 
-
-
-
-/////////////           Useful code below           ///////////
-
-
-// for (var i = 0; i < data.length; i++) {
-//     var listItem = document.createElement('li');
-//     listItem.textContent = data[i].html_url;
-//     repoList.appendChild(listItem);
-// }
-
-
-
-
-
-
-// eventArray[offset] = event;
-// localStorage.setItem("eventArray", JSON.stringify(eventArray))
-
-
-
-
-////////////////  OLD CODE
-
-
 // var fullUrl = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&APPID=530a0c5e5acc988437cc5fa97607f79a"
 
 
@@ -128,13 +100,13 @@ function display() {
     // console.log(requestUrl)
     // fullUrl = requestUrl.concat(currentCity, APIkey)
     // console.log(fullUrl)
-
-// console.log(typeof(searchBtn))
-
+    
+    // console.log(typeof(searchBtn))
+    
 // function test(){
-//     dummy++
-//     console.log("test")
-// }
+    //     dummy++
+    //     console.log("test")
+    // }
 
 
 // $( function() {
@@ -146,39 +118,3 @@ function display() {
 
 
 
-
-
-// function saveCities() {
-//     dummy = 5
-//     console.log(dummy)
-//     var offset = time - 9
-//     userEvent.eventText = eventsArray[offset].textContent;
-//     console.log(userEvent.eventText)
-//     userEvent.eventTime = time;
-
-
-//     // If event were retrieved from localStorage, update the event array to it
-//     if (cityData !== null) {
-//         cityData = fetchedData;
-//     }
-
-
-
-//     // check if empty event and exit early 
-//     if (userEvent.eventText === "") {
-//         if (eventArray[offset] === "") {
-//             eventsArray[offset].textContent = eventArray[offset]
-//         }
-//         console.log('empty');
-//         return;
-//     }
-
-//     localStorage.setItem("userEvent", JSON.stringify(userEvent))
-//     var event = JSON.parse(localStorage.getItem("userEvent"));
-
-//     // Re-write event Only if there's a change
-//     eventArray[offset] = event;
-//     localStorage.setItem("eventArray", JSON.stringify(eventArray))
-
-//     location.reload();
-// }
